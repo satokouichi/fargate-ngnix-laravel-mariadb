4181f1e59f19007d836f37760c684206131f7cf2
kamimura <kamimura@access-jp.co.jp>
2022-04-13 16:30:23 +0900

    1630

--- backend/resources/views/welcome.blade.php
+++ backend/resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>1135</title>
+    <title>1629</title>
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
